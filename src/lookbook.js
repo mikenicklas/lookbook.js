c7826be450f2b7c2ea3d123baa2025009b10d51d
mikenicklas <mike.nicklas@gmail.com>
2016-08-10 20:57:40 -0400

    Support multiple lookbooks on a page. Use passable style options

--- src/lookbook.js
+++ src/lookbook.js
@@ -2,18 +2,19 @@
 
 (function(global, $) {
   var defaults = {
-    backgroundPosition: '100% 0%',
+    imagePosition: '100% 0%',
     point: {
       height: '30px',
       width: '30px',
-      color: '#000',
+      backgroundColor: '#000',
       borderRadius: '50%',
       animationStartColor: 'rgba(255, 255, 255, .5)',
       animationEndColor: 'rgba(0, 0, 0, 0.0)'
     }
   }
 
-  var Lookbook = function(selector) {
+  var Lookbook = function(selector, opts) {
+    this.options = $.extend(true, {}, defaults, opts);
     for(var i = 0; i < selector.length; i++) {
       var $el = $(selector[i]);
       this.createLookbook($el);
@@ -22,7 +23,6 @@
 
   Lookbook.prototype = {
     createLookbook: function ($el) {
-      debugger;
       var image = $el.clone(),
           lbHTML = this._lookbookHTML(image)
        $el.replaceWith(lbHTML);
@@ -40,7 +40,7 @@
           pointsHTML;
 
       for(var i = 0; i < points.length; i++) {
-        var fp = new Lookbook.FocalPoint(points[i]);
+        var fp = new Lookbook.FocalPoint(points[i], this.options);
         pointsArr.push(fp);
       }
 
@@ -53,16 +53,39 @@
         html += arr[i].html;
       }
       return html;
+    },
+
+    _setStyling: function(opts) {
+      $.extend(defaults, opts);
     }
   };
 
-  Lookbook.FocalPoint = function(point) {
-    var style = "position: absolute; height: 30px; width: 30px; border-radius: 50%; background-color: #000;left: " + point.posX + "px; top: " + point.posY + "px";
-    this.html = '<button type="button" class="btn focal-point" style="' + style + '" title="' + point.title + '" data-toggle="popover" data-content="' + point.description + '" data-placement="top"></button>';
+  Lookbook.FocalPoint = function(point, options) {
+    var styleRule = this.styleRule,
+        styles = '',
+        fullElement = '';
+    styles += styleRule('position', 'absolute');
+    styles += styleRule('height', options.point.height);
+    styles += styleRule('width', options.point.width);
+    styles += styleRule('border-radius', options.point.borderRadius);
+    styles += styleRule('background-color', options.point.backgroundColor);
+    styles += styleRule('left', point.posX + 'px');
+    styles += styleRule('top', point.posY + 'px');
+    fullElement = '<button type="button" class="btn focal-point" style="';
+    fullElement += styles + '" title="' + point.title + '" data-toggle="popover" data-content="';
+    fullElement += point.description + '" data-placement="top"></button>';
+    this.html = fullElement;
   };
 
-  $.fn.lookbook = function() {
-    var lb = new Lookbook(this);
+  Lookbook.FocalPoint.prototype = {
+    styleRule: function(attr, val) {
+      return attr + ':' + val + ';';
+    }
+  }
+
+  $.fn.lookbook = function(opts) {
+    var opts = opts || {};
+    var lb = new Lookbook(this, opts);
   };
 
 })(window, $);
